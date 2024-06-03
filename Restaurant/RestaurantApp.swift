56e16b27cb62441e86a9ea8a0d2c69b1e4f9e651
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-06-03 18:02:21 +0530

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by utkarsh mishra on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
