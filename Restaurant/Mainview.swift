d19aca877965d6375d19300b9c05518f3720bb89
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-06-03 18:12:21 +0530

    second view

---
+++ Restaurant/Mainview.swift
@@ -0,0 +1,18 @@
+//
+//  Mainview.swift
+//  Restaurant
+//
+//  Created by utkarsh mishra on 03/06/24.
+//
+
+import SwiftUI
+
+struct Mainview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mainview()
+}
