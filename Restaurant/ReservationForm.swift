ecd84239a16b3cae25786525a8fa0662e5989d3a
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-06-05 16:18:07 +0530

    reservation form

---
+++ Restaurant/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  Restaurant
+//
+//  Created by utkarsh mishra on 05/06/24.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
