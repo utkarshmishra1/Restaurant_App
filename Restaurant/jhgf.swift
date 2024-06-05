a51b638cf037a7d59a127c07528a6ed9dab2d6f1
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-06-05 16:27:42 +0530

    hgfd

---
+++ Restaurant/jhgf.swift
@@ -0,0 +1,18 @@
+//
+//  jhgf.swift
+//  Restaurant
+//
+//  Created by utkarsh mishra on 05/06/24.
+//
+
+import SwiftUI
+
+struct jhgf: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    jhgf()
+}
